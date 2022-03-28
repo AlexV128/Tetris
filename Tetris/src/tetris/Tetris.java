37d502100a27a09f1b389cdaf562085c188a85fc
AlexV128 <alexvasq128@gmail.com>
2022-03-28 20:12:55 +0200

    El hacker hace de las suyas

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -7,6 +7,7 @@ public class Tetris {
         System.out.println("Hola Mundo!");
         System.out.println("Adios Mundo!");
         System.out.println("Segundo Dia de Trabajo");
+        System.out.println("Modificacion hecha desde Linuxs");
     }
     
 }
