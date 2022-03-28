afc6b972f9370b971dd2fcf8ea709d06947d7da1
AlexV128 <alexvasq128@gmail.com>
2022-03-28 19:53:35 +0200

    Commit de mi segundo dia de trabajo

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -6,6 +6,7 @@ public class Tetris {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         System.out.println("Adios Mundo!");
+        System.out.println("Segundo Dia de Trabajo");
     }
     
 }
